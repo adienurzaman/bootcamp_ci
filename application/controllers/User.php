7e658a5a66ad979f76a16fda9665d8935ad6a7f8
Adie Iman Nurzaman <adieimannurzaman@gmail.com>
2021-01-23 04:00:16 +0700

    Frontend only

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,114 +1,18 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller {
+class User extends CI_Controller
+{
 
-	public function __contruct(){
-		parent::__contruct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function index()
-	{	
-		$data['title'] = "Halaman Data User";
-		$data['judul'] = "Setting Data User";
-
-		# Tampilkan data user
-		$sql = "SELECT * FROM tbl_user";
-		$query = $this->db->query($sql);
-
-		# Array asosiatif tbl user
-		$data['user'] = $query->result();
-
-		$this->load->view('user/tampil',$data);
-	}
-
-	public function tambah()
-	{
-		$data['title'] = "Halaman Tambah Data User";
-		$data['judul'] = "Tambah Data User";
-
-		$this->load->view('user/tambah',$data);	
-	}
-
-	public function simpan()
-	{
-		$data = array('username' => $this->input->post('username'),
-					  'password' => md5($this->input->post('password')),
-					  'nama' => $this->input->post('nama'),
-					  'jk' => $this->input->post('jk'),
-					  'alamat' => $this->input->post('alamat'),
-					  'level_user' => $this->input->post('level_user')
-					);
-		$query = $this->db->insert('tbl_user',$data);
-		if($query){
-			echo "<script>alert('Tambah Data Berhasil');</script>";
-			redirect('user');
-		}else{
-			echo "<script>alert('Tambah Data Tidak Berhasil');</script>";
-			redirect('user/tambah');
-		}
-	}
-
-	public function hapus($id){
-		$query = $this->db->delete('tbl_user', array('id_user' => $id));
-		if($query){
-			echo "<script>alert('Hapus Data Berhasil');</script>";
-			redirect('user');
-		}else{
-			echo "<script>alert('Hapus Data Tidak Berhasil');</script>";
-			redirect('user');
-		}
-	}
-
-	public function edit($id){
-		$data['title'] = "Halaman Edit Data User";
-		$data['judul'] = "Edit Data User";
-
-		# mencari data by $id
-		$this->db->from('tbl_user');
-		$this->db->where('id_user',$id);
-		$query = $this->db->get();
-
-		# menurunkan array asosiatif
-		$data['edit'] = $query->row();
-
-		# load view
-		$this->load->view('user/edit',$data);
-
-	}
-
-	public function aksi_ubah()
-	{
-		if(empty($this->input->post('password')))
-		{
-			$data = array('username' => $this->input->post('username'),
-						  'nama' => $this->input->post('nama'),
-						  'jk' => $this->input->post('jk'),
-						  'alamat' => $this->input->post('alamat'),
-						  'level_user' => $this->input->post('level_user')
-						);
-		}
-		else
-		{
-			$data = array('username' => $this->input->post('username'),
-						  'password' => md5($this->input->post('password')),
-						  'nama' => $this->input->post('nama'),
-						  'jk' => $this->input->post('jk'),
-						  'alamat' => $this->input->post('alamat'),
-						  'level_user' => $this->input->post('level_user')
-						);
-		}
-		$query = $this->db->update('tbl_user',$data,array('id_user' => $this->input->post('id_user')));
-		if($query){
-			echo "<script>alert('Ubah Data Berhasil');</script>";
-			redirect('user');
-		}else{
-			echo "<script>alert('Ubah Data Tidak Berhasil');</script>";
-			redirect('user');
-		}
-	}
-
+    public function index()
+    {
+        $this->load->view('user/tampil');
+    }
 }
 
 /* End of file User.php */
-/* Location: ./application/controllers/User.php */
--- application/views/user/tampil.php
+++ application/views/user/tampil.php
@@ -1,15 +1,23 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
-	<title><?= $title; ?></title>
+	<title>Cek</title>
 
 	<style type="text/css">
+		::selection {
+			background-color: #E13300;
+			color: white;
+		}
 
-		::selection { background-color: #E13300; color: white; }
-		::-moz-selection { background-color: #E13300; color: white; }
+		::-moz-selection {
+			background-color: #E13300;
+			color: white;
+		}
 
 		body {
 			background-color: #fff;
@@ -69,6 +77,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
@@ -76,8 +85,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css">
 
 	<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
-
-	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 
 	<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 
@@ -87,72 +94,267 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
 
-	<script type="text/javascript">
-		$(function(){
-			$("#tabel_user").DataTable({
-				responsive: true
-			});
-		});
-	</script>
-
 </head>
+
 <body>
 	<div id="container">
 		<div class="container">
 			<div id="body">
-				<h2><?= $judul; ?></h2>
-				<hr/>
+				<br>
 				<p>
-					<i class="glyphicon glyphicon-user"></i> Data User
-				</p>
-				<p>
-					<a href="<?= base_url('user/tambah'); ?>" class="btn btn-info btn-md">
+					<a class="btn btn-info btn-md" data-toggle="modal" data-target="#modal_tambah">
 						<i class="glyphicon glyphicon-plus"></i> Tambah Data
 					</a>
 				</p>
 				<table id="tabel_user" class="table table-bordered table-striped table-hover nowrap" width="100%">
 					<thead>
 						<tr>
-							<th>No</th>
-							<th>Username</th>
-							<th>Nama</th>
-							<th>Jenis Kelamin</th>
-							<th>Alamat</th>
-							<th>Level User</th>
-							<th>Aksi</th>
+							<th>UserID</th>
+							<th>ID</th>
+							<th>Title</th>
+							<th>Body</th>
+							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody>
-						<?php
-						$no=1;
-						foreach ($user as $data) {
-							echo "<tr>";
-								echo "<td>".$no++."</td>";
-								echo "<td>".$data->username."</td>";
-								echo "<td>".$data->nama."</td>";
-								echo "<td>".$data->jk."</td>";
-								echo "<td>".$data->alamat."</td>";
-								echo "<td>".$data->level_user."</td>";
-								echo "<td>
-									<a href='".base_url('user/edit/'.$data->id_user)."' class='btn btn-md btn-success'>
-										<i class='glyphicon glyphicon-edit'></i> 
-										Edit
-									</a>
-									<a href='".base_url('user/hapus/'.$data->id_user)."' class='btn btn-md btn-danger'>
-										<i class='glyphicon glyphicon-trash'></i> 
-										Hapus
-									</a>
-									</td>";
-							echo "</tr>";
-						}
-						?>
+
 					</tbody>
 				</table>
 			</div>
 
-			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 		</div>
 	</div>
 
+	<div class="modal fade" id="modal_tambah">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Tambah Data</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<form id="form_tambah">
+					<div class="modal-body">
+						<div>
+							<label class="label-control">User ID</label>
+							<input type="number" class="form-control" name="userId" id="userId" required>
+						</div>
+						<div>
+							<label class="label-control">Title</label>
+							<input type="text" class="form-control" name="title" id="title" required>
+						</div>
+						<div>
+							<label class="label-control">Body</label>
+							<textarea class="form-control" name="body" id="body" required></textarea>
+						</div>
+					</div>
+					<div class="modal-footer justify-content-between">
+						<button type="submit" class="btn btn-primary">Simpan</button>
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</form>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<!-- /.modal -->
+
+	<div class="modal fade" id="modal_edit">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Edit Data</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<form id="form_edit">
+					<div class="modal-body">
+						<div>
+							<label class="label-control">User ID</label>
+							<input type="number" class="form-control" name="userId" readonly>
+						</div>
+						<div>
+							<label class="label-control">ID</label>
+							<input type="number" class="form-control" name="id" readonly>
+						</div>
+						<div>
+							<label class="label-control">Title</label>
+							<input type="text" class="form-control" name="title" required>
+						</div>
+						<div>
+							<label class="label-control">Body</label>
+							<textarea class="form-control" name="body" required></textarea>
+						</div>
+					</div>
+					<div class="modal-footer justify-content-between">
+						<button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</form>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<!-- /.modal -->
+
+	<script type="text/javascript">
+		let form_tambah;
+		let form_edit;
+		let modal_tambah;
+		let modal_edit;
+		let tabel;
+		$(function() {
+			form_tambah = $("#form_tambah");
+			form_edit = $("#form_edit");
+			modal_tambah = $("#modal_tambah");
+			modal_edit = $("#modal_edit");
+
+			tabel = $("#tabel_user").DataTable({
+				responsive: true,
+				processing: true,
+				ajax: {
+					url: "https://jsonplaceholder.typicode.com/posts",
+					dataSrc: ""
+				},
+				columns: [{
+						data: "userId"
+					},
+					{
+						data: "id"
+					},
+					{
+						data: "title"
+					},
+					{
+						data: "body"
+					},
+					{
+						render: (data, type, row) => {
+							return '<button class="btn btn-success" onclick=edit("' + row.id + '")>Edit Data</button> <button class="btn btn-danger" onclick=hapus("' + row.id + '")>Hapus Data</button>';
+						}
+					}
+				]
+			});
+
+			//submit data baru
+			form_tambah.on("submit", (event) => {
+				event.preventDefault();
+				let url = "https://jsonplaceholder.typicode.com/posts";
+				let config = {
+					method: "POST",
+					body: JSON.stringify({
+						title: form_tambah.find("[name='title']").val(),
+						body: form_tambah.find("[name='body']").val(),
+						userId: form_tambah.find("[name='userId']").val(),
+					})
+				}
+
+				simpan('tambah', url, config);
+			});
+
+			//submit data edit
+			form_edit.on("submit", (event) => {
+				event.preventDefault();
+				let id = form_edit.find("[name='id']").val();
+				let url = "https://jsonplaceholder.typicode.com/posts/" + id;
+				let config = {
+					method: "PUT",
+					body: JSON.stringify({
+						id: form_edit.find("[name='id']").val(),
+						title: form_edit.find("[name='title']").val(),
+						body: form_edit.find("[name='body']").val(),
+						userId: form_edit.find("[name='userId']").val(),
+					})
+				}
+
+				simpan('update', url, config);
+			});
+		});
+
+		const simpan = (segment, url, config) => {
+			fetch(url, config)
+				.then((response) => {
+					if (response.ok) {
+						return response.json();
+					} else {
+						return Promise.reject({
+							status: response.status,
+							statusText: response.statusText
+						})
+					}
+				}).then((output) => {
+					//response json
+					console.log(output);
+					if (segment == "tambah") {
+						modal_tambah.modal('hide');
+					} else {
+						modal_edit.modal('hide');
+					}
+					tabel.ajax.reload(false, null)
+				}).catch((error) => {
+					console.error(error);
+				})
+		}
+
+		const edit = (id) => {
+			let url = "https://jsonplaceholder.typicode.com/posts/" + id;
+			let config = {
+				method: "GET",
+			}
+			fetch(url, config)
+				.then((response) => {
+					if (response.ok) {
+						return response.json();
+					} else {
+						return Promise.reject({
+							status: response.status,
+							statusText: response.statusText
+						})
+					}
+				}).then((output) => {
+					//response json
+					form_edit.find("[name='userId']").val(output.userId);
+					form_edit.find("[name='id']").val(output.id);
+					form_edit.find("[name='title']").val(output.title);
+					form_edit.find("[name='body']").html(output.body);
+
+					modal_edit.modal({
+						show: true,
+						backdrop: 'static'
+					});
+				}).catch((error) => {
+					console.error(error);
+				})
+		}
+
+		const hapus = (id) => {
+			let url = "https://jsonplaceholder.typicode.com/posts/" + id;
+			let config = {
+				method: "DELETE",
+			}
+			fetch(url, config)
+				.then((response) => {
+					if (response.ok) {
+						return response.text();
+					} else {
+						return Promise.reject({
+							status: response.status,
+							statusText: response.statusText
+						})
+					}
+				}).then((output) => {
+					alert('Hapus Sukses');
+					tabel.ajax.reload(false, null);
+				}).catch((error) => {
+					console.error(error);
+				})
+		}
+	</script>
 </body>
+
 </html>
